e62d2ed6c47fce32c0fe470b8b562f06d4a298a5
Patrick Sullivan <patricksullivan08@gmail.com>
2020-07-22 20:48:28 -0400

    new assign homes

--- src/PXIR/AssignHomes.hs
+++ src/PXIR/AssignHomes.hs
@@ -100,6 +100,8 @@ assignHomesInBlock (Block label instrs) =
   in  (Block label instrs', stackSpace ctx')
   where assignInInstrs = mapM assignHomesInInstr instrs
 
+----------------------------------------------------------
+
 -- unocoverLive -> conflictGraph -> colorGraph
 
 {- | Assign a color, represented by a natural number, to each variable so that
@@ -109,5 +111,57 @@ more colors than necessary.
 assignColors :: [Instr] -> M.Map Var Int
 assignColors = colorGraph . CG.make . liveAfterEach
 
-assignHomes' :: [Instr] -> ([Instr], Int)
-assignHomes' instrs = undefined
+{- | Map each color, represented by a natural number, to an argument. Starting
+from color number zero and working upwards, first assign colors to unique
+registers in the given list of available registers. If all available registers
+are used then assign remaining colors to memory locations on the stack, moving
+progressively further down from the stack base pointer.
+-}
+mapColorToArg :: [Reg] -> M.Map Var Int -> M.Map Var Arg
+mapColorToArg availRegs = M.map
+  (\color -> if color >= length availRegs
+    then
+      let offset = (color - (length availRegs) + 1) * (-8)
+      in  ArgDeref RegRBP offset
+    else ArgReg $ availRegs !! color
+  )
+
+{- | If the argument is a variable then return the new argument to which the
+variable has been assigned.
+
+Fail and error if the argument is a variable that doesn't have a new argument
+assignment.
+-}
+replaceVarInArg :: M.Map Var Arg -> Arg -> Arg
+replaceVarInArg varToArg (ArgVar var) = case M.lookup var varToArg of
+  (Just arg) -> arg
+  Nothing    -> error $ "variable has not been assigned to a home"
+replaceVarInArg _ arg = arg
+
+{- | Replace each variable argument in the instruction with the new argument to
+which the variable has been assigned.
+
+Fail and error if the instruction contains a variable that doesn't have a new
+argument assignment.
+-}
+replaceVarsInInstr :: M.Map Var Arg -> Instr -> Instr
+replaceVarsInInstr varToArg instr = case instr of
+  (InstrAddq src dst) ->
+    let src' = replaceVarInArg varToArg src
+        dst' = replaceVarInArg varToArg dst
+    in  InstrAddq src' dst'
+  (InstrSubq src dst) ->
+    let src' = replaceVarInArg varToArg src
+        dst' = replaceVarInArg varToArg dst
+    in  InstrSubq src' dst'
+  (InstrMovq src dst) ->
+    let src' = replaceVarInArg varToArg src
+        dst' = replaceVarInArg varToArg dst
+    in  InstrMovq src' dst'
+  (InstrNegq dst) -> let dst' = replaceVarInArg varToArg dst in InstrNegq dst'
+  _               -> instr
+
+assignHomes' :: [Reg] -> [Instr] -> ([Instr], Int)
+assignHomes' availRegs instrs =
+  let varToArg = mapColorToArg availRegs $ assignColors instrs
+  in  map (replaceVarsInInstr varToArg) instrs
